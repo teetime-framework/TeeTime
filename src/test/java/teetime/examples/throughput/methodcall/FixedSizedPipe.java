17da2cfde698dab355b0d661ac189c94ab4642b6
Christian Wulf <chw@informatik.uni-kiel.de>
2014-06-17 10:00:10 +0200

    final static ArrayWrapper does not improve performance

--- src/test/java/teetime/examples/throughput/methodcall/FixedSizedPipe.java
+++ src/test/java/teetime/examples/throughput/methodcall/FixedSizedPipe.java
@@ -2,8 +2,35 @@ package teetime.examples.throughput.methodcall;
 
 public class FixedSizedPipe<T> implements IPipe<T> {
 
+	// private static final class ArrayWrapper2<T> {
+	//
+	// private final T[] elements;
+	//
+	// // private int lastFreeIndex;
+	//
+	// @SuppressWarnings("unchecked")
+	// public ArrayWrapper2(final int initialCapacity) {
+	// super();
+	// this.elements = (T[]) new Object[initialCapacity];
+	// }
+	//
+	// public final T get(final int index) {
+	// return this.elements[index];
+	// }
+	//
+	// public final void put(final int index, final T element) {
+	// this.elements[index] = element;
+	// }
+	//
+	// public final int getCapacity() {
+	// return this.elements.length;
+	// }
+	//
+	// }
+
+	@SuppressWarnings("unchecked")
 	private final T[] elements = (T[]) new Object[4];
-	// private final ArrayWrapper<T> elements = new ArrayWrapper<T>(2);
+	// private final ArrayWrapper2<T> elements = new ArrayWrapper2<T>(2);
 	private int lastFreeIndex;
 
 	public static <T> void connect(final OutputPort<T> sourcePort, final InputPort<T> targetPort) {
