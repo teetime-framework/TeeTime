236f22de76a200e4e14305da6f3cf7ffa7facf7f
Christian Wulf <chw@informatik.uni-kiel.de>
2014-08-27 16:37:16 +0200

    updated performance assertions

--- src/test/java/teetime/variant/methodcallWithPorts/examples/ChwWorkComparisonMethodcallWithPorts.java
+++ src/test/java/teetime/variant/methodcallWithPorts/examples/ChwWorkComparisonMethodcallWithPorts.java
@@ -81,8 +81,16 @@ public class ChwWorkComparisonMethodcallWithPorts implements PerformanceCheckPro
 		// assertEquals(53, value17, 4.1); // +14
 
 		// since 04.07.2014 (incl.)
-		assertEquals(86, value14, 5.1); // +16
-		assertEquals(26, value10, 2.1); // +0
+		// assertEquals(86, value14, 5.1); // +16
+		// assertEquals(26, value10, 2.1); // +0
+		// assertEquals(41, value11, 4.1); // -3
+		// assertEquals(42, value9, 2.1); // +6
+		// assertEquals(44, value15, 4.1); // +0
+		// assertEquals(53, value17, 4.1); // +0
+
+		// since 27.08.2014 (incl.)
+		assertEquals(112, value14, 5.1); // +16
+		assertEquals(42, value10, 2.1); // +16
 		assertEquals(41, value11, 4.1); // -3
 		assertEquals(42, value9, 2.1); // +6
 		assertEquals(44, value15, 4.1); // +0
