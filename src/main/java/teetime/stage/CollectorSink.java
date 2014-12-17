9609394546f66e07d8c1fa94fdc9fa22899f43b7
Christian Wulf <chw@informatik.uni-kiel.de>
2014-12-17 07:37:31 +0100

    uncommented maven checkstyle plugin

--- src/main/java/teetime/stage/CollectorSink.java
+++ src/main/java/teetime/stage/CollectorSink.java
@@ -1,18 +1,3 @@
-/***************************************************************************
- * Copyright 2014 Kieker Project (http://kieker-monitoring.net)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- ***************************************************************************/
 package teetime.stage;
 
 import java.util.ArrayList;
@@ -21,6 +6,9 @@ import java.util.List;
 import teetime.framework.AbstractConsumerStage;
 
 /**
+ * @param <T>
+ *            the type of the elements to be collected
+ *
  * @author Christian Wulf
  *
  * @since 1.0
@@ -30,7 +18,7 @@ public final class CollectorSink<T> extends AbstractConsumerStage<T> {
 	private final List<T> elements;
 
 	/**
-	 * Creates a new {@link CollectorSink} with an {@link ArrayList}.
+	 * Creates a new {@link CollectorSink} with an empty {@link ArrayList}.
 	 */
 	public CollectorSink() {
 		this(new ArrayList<T>());
