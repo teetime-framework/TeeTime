c1eaff5f35e9a43579763ddb6973ad8f657c9932
Nelson Tavares de Sousa <ntd@informatik.uni-kiel.de>
2015-01-30 15:19:32 +0100

    empty test classes

---
+++ src/test/java/teetime/framework/ExceptionHandling.java
@@ -0,0 +1,14 @@
+package teetime.framework;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExceptionHandling {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/teetime/framework/exceptionHandling/TestListener.java
@@ -0,0 +1,13 @@
+package teetime.framework.exceptionHandling;
+
+import teetime.framework.Stage;
+
+public class TestListener extends StageExceptionListener {
+
+	@Override
+	public FurtherExecution onStageException(Exception e, Stage throwingStage) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
