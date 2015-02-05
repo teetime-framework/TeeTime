ef2e56db3b1fd0be26187741312900a8f70f29c7
Nelson Tavares de Sousa <ntd@informatik.uni-kiel.de>
2015-02-05 21:31:51 +0100

    introduced new exception class for analysis

--- src/main/java/teetime/framework/Analysis.java
+++ src/main/java/teetime/framework/Analysis.java
@@ -193,7 +193,7 @@ public class Analysis implements UncaughtExceptionHandler {
 	public void execute() {
 		start();
 		if (!exceptions.isEmpty()) {
-			throw new RuntimeException("Errors while running analysis"); // TODO: add exceptions
+			throw new AnalysisException(exceptions); // TODO: add exceptions
 		}
 	}
 
