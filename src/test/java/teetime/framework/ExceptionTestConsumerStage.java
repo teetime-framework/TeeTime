74673d4f046eb1ab5784b62ffec1f6dbf8813393
Nelson Tavares de Sousa <ntd@informatik.uni-kiel.de>
2015-02-06 09:44:09 +0100

    forgot to add test class

---
+++ src/test/java/teetime/framework/ExceptionTestConsumerStage.java
@@ -0,0 +1,15 @@
+package teetime.framework;
+
+public class ExceptionTestConsumerStage extends AbstractConsumerStage<Object> {
+
+	private int numberOfExecutions = 0;
+
+	@Override
+	protected void execute(final Object element) {
+		if (numberOfExecutions % 1000 == 0) {
+			throw new IllegalStateException("1000 loops");
+		}
+		numberOfExecutions++;
+	}
+
+}
