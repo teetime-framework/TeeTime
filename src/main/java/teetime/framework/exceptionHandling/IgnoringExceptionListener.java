86fd745d9b57bfec0a296dc7e32cf09f3a27b98e
Nelson Tavares de Sousa <ntd@informatik.uni-kiel.de>
2015-03-27 13:57:20 +0100

    removed constructor

--- src/main/java/teetime/framework/exceptionHandling/IgnoringExceptionListener.java
+++ src/main/java/teetime/framework/exceptionHandling/IgnoringExceptionListener.java
@@ -4,10 +4,6 @@ import teetime.framework.Stage;
 
 public class IgnoringExceptionListener extends AbstractExceptionListener {
 
-	public IgnoringExceptionListener() {
-		super();
-	}
-
 	@Override
 	public FurtherExecution onStageException(final Exception e, final Stage throwingStage) {
 		return FurtherExecution.CONTINUE;
