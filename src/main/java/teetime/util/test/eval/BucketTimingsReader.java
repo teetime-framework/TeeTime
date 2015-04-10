f85dbce6e6044edb021e5e7d70e6efabaca49f63
Nelson Tavares de Sousa <ntd@informatik.uni-kiel.de>
2015-04-10 12:44:48 +0200

    modified license and header-plugin

--- src/main/java/teetime/util/test/eval/BucketTimingsReader.java
+++ src/main/java/teetime/util/test/eval/BucketTimingsReader.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2015 TeeTime (http://teetime.sourceforge.net)
+ * Copyright (C) 2015 Christian Wulf, Nelson Tavares de Sousa (http://teetime.sourceforge.net)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
