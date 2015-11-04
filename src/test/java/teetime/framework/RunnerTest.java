66a60cd5f46dcc357f888cdafb368468fdc2c0df
Nelson Tavares de Sousa <stu103017@mail.uni-kiel.de>
2015-11-04 15:37:12 +0100

    first (very simple) test

---
+++ src/test/java/teetime/framework/RunnerTest.java
@@ -0,0 +1,12 @@
+package teetime.framework;
+
+import org.junit.Test;
+
+public class RunnerTest {
+
+	@Test
+	public void test() {
+		Execution.main(new String[] { "teetime.framework.CompositeProducerConfig" });
+	}
+
+}
