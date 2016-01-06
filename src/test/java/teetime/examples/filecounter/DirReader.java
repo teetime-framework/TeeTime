fc1d5493add4f66173a4eb174426ff30779dc156
Nelson Tavares de Sousa <stu103017@mail.uni-kiel.de>
2016-01-06 15:15:13 +0100

    javadoc and some other minor things

--- src/test/java/teetime/examples/filecounter/DirReader.java
+++ src/test/java/teetime/examples/filecounter/DirReader.java
@@ -1,9 +1,30 @@
+/**
+ * Copyright (C) 2015 Christian Wulf, Nelson Tavares de Sousa (http://teetime-framework.github.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package teetime.examples.filecounter;
 
 import java.io.File;
 
 import teetime.framework.AbstractProducerStage;
 
+/**
+ * Visits the given directory and all containing sub-directories and sends all containing files to its output port.
+ *
+ * @author Nelson Tavares de Sousa
+ *
+ */
 class DirReader extends AbstractProducerStage<File> {
 
 	private final File element;
@@ -22,6 +43,12 @@ class DirReader extends AbstractProducerStage<File> {
 		this.terminate(); // If everything is done, terminate
 	}
 
+	/**
+	 * Helper method to enable recursive calls
+	 *
+	 * @param element
+	 *            Dir to visit
+	 */
 	private void visit(final File element) {
 		for (File file : element.listFiles()) {
 			if (file.isDirectory()) {
