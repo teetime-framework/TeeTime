5f2726fcc561b364fcc017eec51558cc908cef76
Nelson Tavares de Sousa <stu103017@mail.uni-kiel.de>
2016-01-06 15:29:24 +0100

    added a new news site for testing

---
+++ src/site/resources/js/twitter_news.js
@@ -0,0 +1,4 @@
+!function ($) {
+	$('.twitter-timeline').attr('data-widget-id', '676369168816578560');
+	$('.twitter-timeline').attr('data-chrome', 'noborders nofooter transparent noscrollbar');
+}(window.jQuery)
