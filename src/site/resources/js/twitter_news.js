f47cf49a96b555faa56e23a27a3fbb40ebcc0c20
Nelson Tavares de Sousa <stu103017@mail.uni-kiel.de>
2016-01-06 17:00:38 +0100

    added scrollbar to widget

--- src/site/resources/js/twitter_news.js
+++ src/site/resources/js/twitter_news.js
@@ -1,4 +1,4 @@
 !function ($) {
 	$('.twitter-timeline').attr('data-widget-id', '676369168816578560');
-	$('.twitter-timeline').attr('data-chrome', 'noborders nofooter transparent noscrollbar');
+	$('.twitter-timeline').attr('data-chrome', 'noborders nofooter transparent');
 }(window.jQuery)
