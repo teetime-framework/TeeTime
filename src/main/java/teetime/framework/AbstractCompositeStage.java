62259bcd9a9cc1cf4b9b201efdd7f34fea6c6eba
Nelson Tavares de Sousa <stu103017@mail.uni-kiel.de>
2016-02-02 16:16:13 +0100

    added legacy code for api change

---
+++ src/main/java/teetime/framework/AbstractCompositeStage.java
@@ -0,0 +1,16 @@
+package teetime.framework;
+
+/**
+ * Legacy class: was renamed to {@link CompositeStage}
+ *
+ * @author Nelson Tavares de Sousa
+ *
+ */
+@Deprecated
+public class AbstractCompositeStage extends CompositeStage {
+
+	protected AbstractCompositeStage() {
+		super();
+	}
+
+}
