348f476a4a38c51ca7f51e44f077b0763ea4873b
Christian Wulf <chw@informatik.uni-kiel.de>
2017-12-19 14:22:34 +0100

    added IteratorProducer

---
+++ src/main/java/teetime/stage/IteratorProducer.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright © 2015 Christian Wulf, Nelson Tavares de Sousa (http://teetime-framework.github.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package teetime.stage;
+
+import java.util.Iterator;
+import java.util.stream.BaseStream;
+
+import teetime.framework.AbstractProducerStage;
+
+/**
+ * @author Christian Wulf
+ *
+ * @since 3.0
+ */
+public class IteratorProducer<T> extends AbstractProducerStage<T> {
+
+	private final BaseStream<T, ?> stream;
+
+	/**
+	 * @param stream
+	 *            a stream which creates new instances of type <code>T</code>.
+	 */
+	public IteratorProducer(final BaseStream<T, ?> stream) {
+		if (stream == null) {
+			throw new IllegalArgumentException("stream may not be null");
+		}
+		this.stream = stream;
+	}
+
+	@Override
+	protected void execute() {
+		Iterator<T> iterator = stream.iterator();
+		while (iterator.hasNext()) {
+			T newObject = iterator.next();
+			outputPort.send(newObject);
+		}
+		this.terminateStage();
+	}
+
+}
