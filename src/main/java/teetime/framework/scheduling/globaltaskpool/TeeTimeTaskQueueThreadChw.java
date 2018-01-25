5b98fa23f861add70165aabc5c5f9bd46e9c9bf0
Christian Wulf <chw@informatik.uni-kiel.de>
2018-01-25 11:00:47 +0100

    updated license header; maven-pmd-plugin now downloads and uses our custom rules from sonatype

--- src/main/java/teetime/framework/scheduling/globaltaskpool/TeeTimeTaskQueueThreadChw.java
+++ src/main/java/teetime/framework/scheduling/globaltaskpool/TeeTimeTaskQueueThreadChw.java
@@ -1,5 +1,5 @@
 /**
- * Copyright © 2017 Christian Wulf, Nelson Tavares de Sousa (http://teetime-framework.github.io)
+ * Copyright © 2015 Christian Wulf, Nelson Tavares de Sousa (http://teetime-framework.github.io)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
